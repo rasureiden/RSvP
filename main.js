95f96198dcf35b661e0838a61d861d08dcc1354c
rasureiden <amhou@ucsc.edu>
2022-10-10 18:43:24 -0700

    characters fixed, text needs to be smaller

--- main.js
+++ main.js
@@ -5,45 +5,65 @@ description = `
 `;
 
 characters = [
-  `y  y
-  yyyyyy
-   yyyy 
-  yyyyyy
-   yyyy 
-   y  y`
-  ,
-  `P  P
-  PPPPPP
-  PPPPPP
-   PPPP
-  PPPPPP
-  p    p`
-  ,
+`
+ y  y 
+yyyyyy
+ yyyy  
+yyyyyy
+ yyyy  
+ y  y 
+`
+,
+`
+ P  P 
+PPPPPP
+PPPPPP
+ PPPP 
+PPPPPP
+P    P
+`
+,
+
 ];
 
 const G = {
   WIDTH: 100,
-  HEIGHT: 150,
+  HEIGHT: 110,
+
+  //TALK_SPEED: 3
 };
 
 options = {
   viewSize: {x: G.WIDTH, y: G.HEIGHT},
   isPlayingBGM: true,
   isReplayEnabled: true,
+  isShowingScore: false,
 };
 
 function update() {
   if (!ticks) {
-    // text! -> "text(string [what you want text to say], x [x coord of text], y [y coord], options? [see definitions?])"  
-    // text(string, x, y, options?); text(string, p, options?); <- from https://kandi.openweaver.com/typescript/abagames/crisp-game-lib#Code-Snippets
-    text("are you awake...?", 10, 20);
-    // draw characters! 
-    // char(string, x, y, options?);
-    // char(string, p, options?);
-    char("a", 30, 40);
-}
     
   }
+  // draw characters! 
+  // char(string, x, y, options?);
+  // char(string, p, options?);
+  char("a", 30, 40);
+  char("b", 60, 40);
+  // text! -> "text(string [what you want text to say], x [x coord of text], y [y coord], options? [see definitions?])"  
+  // text(string, x, y, options?); text(string, p, options?); <- from https://kandi.openweaver.com/typescript/abagames/crisp-game-lib#Code-Snippets
+  text("are you awake...?", 10, 20);
+}
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * }} Player
+ */
+
+/**
+ * @type { Player }
+ */
+let player;
 
 
 addEventListener("load", onLoad);
